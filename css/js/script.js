bb801d3148c6377539feef0eb9da9a33362ba8c6
unknown <ivan.ananda20@mhs.uinjkt.ac.id>
2023-03-18 19:45:27 +0700

    week 1

---
+++ css/js/script.js
@@ -0,0 +1,18 @@
+//Toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika hamburger di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active')
+};
+ 
+// klik di luar side bar untuk menghilangkan nav
+
+const hamburger =document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.targe)){
+    navbarNav.classList.remove('active');
+  }
+})
